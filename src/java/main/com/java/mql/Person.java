0ed26097ca6ddad6e19761c809f2574f84f57046
abdallah <abdallah.eddehbi@gmail.com>
2020-06-07 15:33:09 +0200

    STR-2 add constructor and getters to Person

--- src/java/main/com/java/mql/Person.java
+++ src/java/main/com/java/mql/Person.java
@@ -2,4 +2,21 @@ public class Person{
   private int age;
   private String firstName;
   private String lastName;
+
+  public Person(int age, String firstName, String lastName)
+  {
+    this.age=age;
+    this.firstName=firstName;
+    this.lastName=lastName;
+  }
+  public int getAge()
+  {
+    return age;
+  }
+  public String getFirstName(){
+    return firstName;
+  }
+  public String getLastName(){
+    return lastName;
+  }
 }
